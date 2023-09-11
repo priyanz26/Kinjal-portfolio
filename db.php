e3d9a6095f20e45075f029cf96ac4c0b1270267b
Priyansh Jadav <116375679+priyanz26@users.noreply.github.com>
2023-09-11 21:08:38 +0530

    Add files via upload

---
+++ db.php
@@ -0,0 +1,9 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "priyansh";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+?>
